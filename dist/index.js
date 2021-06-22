528cf583dd8e26825296c68098dbf48e44a653ae
Ben Drucker <bvdrucker@gmail.com>
2021-06-22 11:15:51 -0700

    build

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,56 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 548:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.modules = void 0;
+const glob = __importStar(__nccwpck_require__(90));
+const path = __importStar(__nccwpck_require__(622));
+function modules(wd, options) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const globber = yield glob.create([path.join(wd, '**', '*.tf'), '!**/.terraform/**/*'].join('\n'), options);
+        const files = yield globber.glob();
+        const mods = new Set(files.map(file => path.dirname(file)).sort((a, b) => (a > b ? 1 : -1)));
+        return [...mods];
+    });
+}
+exports.modules = modules;
+
+
+/***/ }),
+
 /***/ 109:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -36,16 +86,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(186));
-const glob = __importStar(__nccwpck_require__(90));
-const path = __importStar(__nccwpck_require__(622));
+const find = __importStar(__nccwpck_require__(548));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const wd = core.getInput('working-directory');
-            const globber = yield glob.create(path.join(wd, '**', '*.tf'));
-            const files = yield globber.glob();
-            const modules = new Set(files.map(file => path.dirname(file)).sort((a, b) => (a > b ? 1 : -1)));
-            core.setOutput('modules', Array.from(modules));
+            core.setOutput('modules', yield find.modules(wd, {
+                followSymbolicLinks: core.getInput('follow-symbolic-links').toUpperCase() !== 'FALSE'
+            }));
         }
         catch (err) {
             core.setFailed(err.message);
