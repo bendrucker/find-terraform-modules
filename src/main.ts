cec790d0b46d032d11543eb9026cec8931bb7c1a
Ben Drucker <bvdrucker@gmail.com>
2021-06-22 10:34:27 -0700

    format

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import * as core from '@actions/core'
-import * as glob from "@actions/glob"
-import * as path from "path"
+import * as glob from '@actions/glob'
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
     const wd = core.getInput('working-directory')
-    
-    const globber = await glob.create(path.join(wd, "**", "*.tf"))
+
+    const globber = await glob.create(path.join(wd, '**', '*.tf'))
     const files = await globber.glob()
 
-    const modules = new Set(files.map(path.dirname).sort())
+    const modules = new Set(
+      files.map(file => path.dirname(file)).sort((a, b) => (a > b ? 1 : -1))
+    )
 
-    core.setOutput("modules", Array.from(modules))
+    core.setOutput('modules', Array.from(modules))
   } catch (err) {
     core.setFailed(err.message)
   }
